cdca1f5c679f6ecb454522583ffd580c20908a6b
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-30 14:33:32 +0000

    Removed bug: cannot call .count on non created csv file

--- lib/account.rb
+++ lib/account.rb
@@ -2,10 +2,11 @@ require 'csv'
 
 class Account
   attr_reader :csv
+
   def initialize
     @balance = 0
     @csv = CSV.open("statement.csv", "a+") do |hdr|
-      hdr << ["date","credit","debit","balance\n"] if csv.count.eql? 0
+      hdr << ["date","credit","debit","balance\n"]
     end
   end
 
