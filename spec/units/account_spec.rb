27624cc34d9ba020052d8957f4efa141c18671cb
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-30 12:14:26 +0000

    Deleted #initialize test as #balance test covers the same point

--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -3,12 +3,6 @@ require 'account'
 describe Account do
   subject(:account) { described_class.new }
 
-  describe "#initialize" do
-    it 'initializes with a balance of 0' do
-      expect(account.balance).to eq(0)
-    end
-  end
-
   describe "#balance" do 
     it "returns the balance" do
       expect(account.check_balance()).to eq(0)
