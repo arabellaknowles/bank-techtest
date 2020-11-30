4aafe696d1c7297e4881aff792e79fa90e3225c6
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-30 17:28:04 +0000

    deleted add_to_statement_array method in bank account class

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -8,14 +8,12 @@ class BankStatement
   private
 
   def load(filename = "statement.csv")
-    require 'csv'
     row_array = []
     statement_array = []
 
     CSV.foreach(filename) do |row|
       date, credit, debit, balance = row
-      create_row_array(date, credit, debit, balance)
-      add_to_statement_array(row_array)
+      statement_array << create_row_array(date, credit, debit, balance)
     end
 
     statement_array
@@ -23,10 +21,6 @@ class BankStatement
 
   def create_row_array(date, credit, debit, balance)
     row_array = [date, credit, debit, balance]
-  end
-
-  def add_to_statement_array(row_array)
-    statement_array << row_array
   end
 
   def format_table
