d96e7557152fdd21e93f6584b26e293e309a23fb
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-01 11:11:08 +0000

    Updated feature tests to use new printer class

--- spec/features/deposit_and_withdrawal_spec.rb
+++ spec/features/deposit_and_withdrawal_spec.rb
@@ -6,8 +6,10 @@ describe "Deposits and withdrawals are shown on bank statement" do
     ellas_account = Account.new
     ellas_account.deposit(2000)
     ellas_account.withdraw(100)
-    ellas_statement = BankStatement.new
-    expect(ellas_statement.print).to eq("date || credit || debit || balance\n01/12/2020 || 2000 ||  || 2000\n01/12/2020 ||  || 100 || 1900")
+    ellas_statement = BankStatement.new(ellas_account)
+    ellas_printer = Printer.new(ellas_statement)
+    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 2000 ||  || 2000\n01/12/2020 ||  || 100 || 1900")
+    ellas_printer.print
   end
 end
 describe "Multiple accounts created" do
@@ -18,7 +20,9 @@ describe "Multiple accounts created" do
     bellas_account = Account.new
     bellas_account.deposit(3000)
     bellas_account.withdraw(100)
-    bellas_statement = BankStatement.new
-    expect(bellas_statement.print).to eq("date || credit || debit || balance\n01/12/2020 || 3000 ||  || 3000\n01/12/2020 ||  || 100 || 2900")
+    bellas_statement = BankStatement.new(bellas_account)
+    bellas_printer = Printer.new(bellas_statement)
+    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 3000 ||  || 3000\n01/12/2020 ||  || 100 || 2900")
+    bellas_printer.print
   end
 end
