5421072137bf8c014feac50d1e0f9e760ad561b8
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-01 10:58:17 +0000

    Printers #print method outputs to stdout

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,7 +1,11 @@
 class Printer
   attr_reader :formatted_bank_statement
-  
+
   def initialize(bank_statement)
     @formatted_bank_statement = bank_statement.format
+  end
+
+  def print
+    puts @formatted_bank_statement
   end
 end
--- spec/units/printer_spec.rb
+++ spec/units/printer_spec.rb
@@ -4,11 +4,19 @@ describe Printer do
   let(:statement) { double('bank_statement') }
   subject(:printer) { described_class.new(statement) }
 
+  before do
+    allow(statement).to receive(:format).and_return("date || credit || debit || balance\n01/12/2020 || 20 ||  || 20\n01/12/2020 ||  || 10 || 10")
+  end
+
   describe '#initialize' do
     it 'initializes with a formatted bankstatement' do
-      allow(statement).to receive(:format).and_return("date || credit || debit || balance\n01/12/2020 || 20 ||  || 20\n01/12/2020 ||  || 10 || 10")
       expect(printer.formatted_bank_statement).to eq("date || credit || debit || balance\n01/12/2020 || 20 ||  || 20\n01/12/2020 ||  || 10 || 10")
     end
   end
 
+  describe '#print' do
+    it 'prints out the bank statement' do
+      expect { printer.print }.to output.to_stdout 
+    end
+  end
 end
