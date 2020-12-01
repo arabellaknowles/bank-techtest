a58d132bbb466aec3df3f32f419a29c541f56030
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-01 11:14:42 +0000

    Added new feature test to test printing out the statement of a previous user

--- spec/features/deposit_and_withdrawal_spec.rb
+++ spec/features/deposit_and_withdrawal_spec.rb
@@ -2,7 +2,7 @@ require 'account'
 require 'bank_statement'
 
 describe "Deposits and withdrawals are shown on bank statement" do
-  it "User creates new account, deposits £100, withdraws £50 and prints statement" do
+  it "User creates new account, deposits £2000, withdraws £100 and prints statement" do
     ellas_account = Account.new
     ellas_account.deposit(2000)
     ellas_account.withdraw(100)
@@ -12,8 +12,9 @@ describe "Deposits and withdrawals are shown on bank statement" do
     ellas_printer.print
   end
 end
+
 describe "Multiple accounts created" do
-  it "Users create new account, deposits £100, withdraws £50 and prints statement" do
+  it "Users create new accounts, second user deposits £3000, withdraws £100 and prints statement" do
     ellas_account = Account.new
     ellas_account.deposit(2000)
     ellas_account.withdraw(100)
@@ -24,5 +25,19 @@ describe "Multiple accounts created" do
     bellas_printer = Printer.new(bellas_statement)
     expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 3000 ||  || 3000\n01/12/2020 ||  || 100 || 2900")
     bellas_printer.print
+  end
+  
+  it "Users create new accounts, first user deposits and withdraws, second user deposits and withdraws, first users statement is printed" do
+    ellas_account = Account.new
+    ellas_account.deposit(2000)
+    ellas_account.withdraw(100)
+    bellas_account = Account.new
+    bellas_account.deposit(3000)
+    bellas_account.withdraw(100)
+    bellas_statement = BankStatement.new(bellas_account)
+    ellas_statement = BankStatement.new(ellas_account)
+    ellas_printer = Printer.new(ellas_statement)
+    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 2000 ||  || 2000\n01/12/2020 ||  || 100 || 1900")
+    ellas_printer.print
   end
 end
