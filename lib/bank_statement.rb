e0446d7e81956d7a864f8995da1f4fdf7eca58dd
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-01 10:33:14 +0000

    deleted bank account private methods relating to csv file no longer in use

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -15,28 +15,4 @@ class BankStatement
     "date || credit || debit || balance\n" + statement_array.join( "\n" )
   end
 
-  private
-
-  def load(filename = "statement.csv")
-    row_array = []
-    statement_array = []
-
-    CSV.foreach(filename) do |row|
-      date, credit, debit, balance = row
-      statement_array << create_row_array(date, credit, debit, balance)
-    end
-
-    statement_array
-  end
-
-  def create_row_array(date, credit, debit, balance)
-    row_array = [date, credit, debit, balance]
-  end
-
-  def format_table
-    statement_array = load.map do |row_array|
-      row_array.join( " || " )
-    end
-    statement_array.join( "\n" )
-  end
 end
