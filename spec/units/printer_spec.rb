d2d414fe8ddec21e99e6a9f26eb8f4e99bc36fc7
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-02 11:52:23 +0000

    Updated stubbed account transactions to have two decimal places on numbers in bank statement and printer spec

--- spec/units/printer_spec.rb
+++ spec/units/printer_spec.rb
@@ -5,12 +5,12 @@ describe Printer do
   subject(:printer) { described_class.new(statement) }
 
   before do
-    allow(statement).to receive(:format).and_return("date || credit || debit || balance\n03/12/2020 || 20 ||  || 20\n03/12/2020 ||  || 10 || 10")
+    allow(statement).to receive(:format).and_return("date || credit || debit || balance\n03/12/2020 || 20.00 ||  || 20.00\n03/12/2020 ||  || 10.00 || 10.00")
   end
 
   describe '#print' do
     it 'prints out the bank statement' do
-      expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n03/12/2020 || 20 ||  || 20\n03/12/2020 ||  || 10 || 10")
+      expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n03/12/2020 || 20.00 ||  || 20.00\n03/12/2020 ||  || 10.00 || 10.00")
       printer.print
     end
   end
