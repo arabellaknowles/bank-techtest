deadc1403d32c7060551462cc96c4918c170a00a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-12-02 11:22:07 +0000

    Stubbed account date method in feature test to return specific date

--- spec/features/deposit_and_withdrawal_spec.rb
+++ spec/features/deposit_and_withdrawal_spec.rb
@@ -1,43 +1,49 @@
 require 'account'
 require 'bank_statement'
 
-describe 'Deposits and withdrawals are shown on bank statement' do
-  it 'User creates new account, deposits £2000, withdraws £100 and prints statement' do
-    ellas_account = Account.new
-    ellas_account.deposit(2000)
-    ellas_account.withdraw(100)
-    ellas_statement = BankStatement.new(ellas_account)
-    ellas_printer = Printer.new(ellas_statement)
-    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 2000 ||  || 2000\n01/12/2020 ||  || 100 || 1900")
-    ellas_printer.print
-  end
-end
-
-describe 'Multiple accounts created' do
-  it 'Users create new accounts, first user deposits and withdraws, second user deposits £3000, withdraws £100 and prints statement' do
-    ellas_account = Account.new
-    ellas_account.deposit(2000)
-    ellas_account.withdraw(100)
-    bellas_account = Account.new
-    bellas_account.deposit(3000)
-    bellas_account.withdraw(100)
-    bellas_statement = BankStatement.new(bellas_account)
-    bellas_printer = Printer.new(bellas_statement)
-    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 3000 ||  || 3000\n01/12/2020 ||  || 100 || 2900")
-    bellas_printer.print
+describe 'Features' do
+  before do
+    allow_any_instance_of(Account).to receive(:date).and_return('03/12/2020')
   end
 
-  it 'Users create new accounts, first user deposits and withdraws, second user deposits and withdraws, first users statement is printed' do
-    ellas_account = Account.new
-    ellas_account.deposit(2000)
-    ellas_account.withdraw(100)
-    bellas_account = Account.new
-    bellas_account.deposit(3000)
-    bellas_account.withdraw(100)
-    bellas_statement = BankStatement.new(bellas_account)
-    ellas_statement = BankStatement.new(ellas_account)
-    ellas_printer = Printer.new(ellas_statement)
-    expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n01/12/2020 || 2000 ||  || 2000\n01/12/2020 ||  || 100 || 1900")
-    ellas_printer.print
+  describe 'Deposits and withdrawals are shown on bank statement' do
+    it 'User creates new account, deposits £2000, withdraws £100 and prints statement' do
+      ellas_account = Account.new
+      ellas_account.deposit(2000)
+      ellas_account.withdraw(100)
+      ellas_statement = BankStatement.new(ellas_account)
+      ellas_printer = Printer.new(ellas_statement)
+      expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n03/12/2020 || 2000 ||  || 2000\n03/12/2020 ||  || 100 || 1900")
+      ellas_printer.print
+    end
+  end
+
+  describe 'Multiple accounts created' do
+    it 'Users create new accounts, first user deposits and withdraws, second user deposits £3000, withdraws £100 and prints statement' do
+      ellas_account = Account.new
+      ellas_account.deposit(2000)
+      ellas_account.withdraw(100)
+      bellas_account = Account.new
+      bellas_account.deposit(3000)
+      bellas_account.withdraw(100)
+      bellas_statement = BankStatement.new(bellas_account)
+      bellas_printer = Printer.new(bellas_statement)
+      expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n03/12/2020 || 3000 ||  || 3000\n03/12/2020 ||  || 100 || 2900")
+      bellas_printer.print
+    end
+
+    it 'Users create new accounts, first user deposits and withdraws, second user deposits and withdraws, first users statement is printed' do
+      ellas_account = Account.new
+      ellas_account.deposit(2000)
+      ellas_account.withdraw(100)
+      bellas_account = Account.new
+      bellas_account.deposit(3000)
+      bellas_account.withdraw(100)
+      bellas_statement = BankStatement.new(bellas_account)
+      ellas_statement = BankStatement.new(ellas_account)
+      ellas_printer = Printer.new(ellas_statement)
+      expect(STDOUT).to receive(:puts).with("date || credit || debit || balance\n03/12/2020 || 2000 ||  || 2000\n03/12/2020 ||  || 100 || 1900")
+      ellas_printer.print
+    end
   end
 end
